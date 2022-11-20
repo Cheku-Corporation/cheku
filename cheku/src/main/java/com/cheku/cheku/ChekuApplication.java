a18b91d0c49a79de0ed772b0ccd59b0e33d2c8fb
Emanuel <emanuel.gmarques@ua.pt>
2022-11-20 14:44:38 +0000

    created spring boot project

---
+++ cheku/src/main/java/com/cheku/cheku/ChekuApplication.java
@@ -0,0 +1,13 @@
+package com.cheku.cheku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChekuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChekuApplication.class, args);
+	}
+
+}
